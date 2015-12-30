913e7d4933e73ded5fc2df37e91aba5bf8978caf
Huy Le <huyle@atlassian.com>
2015-12-31 06:10:03 +1100

    use logarithm for scoring

--- src/main/scala/NaiveBayes.scala
+++ src/main/scala/NaiveBayes.scala
@@ -9,49 +9,47 @@ class NaiveBayes[C](debug: Boolean = false) {
 
    var allKlassInfo = new HashMap[C,KlassInfo]
    var vocabulary = new HashSet[String]
-   var nTerms = 0
    var nDocs = 0
 
    def train(klass: C, doc: Iterable[String]) = {
-     if( !allKlassInfo.contains(klass) ){
-       allKlassInfo += (klass->KlassInfo(0,0,new HashMap[String,Int]))
+     if (!allKlassInfo.contains(klass)) {
+       allKlassInfo += (klass -> KlassInfo(0,0,new HashMap[String,Int]))
      }
 
      val klassInfo = allKlassInfo(klass)
      klassInfo.nDocs += 1
      nDocs += 1
      doc.foreach { term =>
-       if( !klassInfo.termFreq.contains(term) )
-         klassInfo.termFreq += (term->1)
+       if (!klassInfo.termFreq.contains(term))
+         klassInfo.termFreq += (term -> 1)
        else
          klassInfo.termFreq(term) += 1
        klassInfo.nTerms += 1
 
        vocabulary += term
-       nTerms += 1
      }
    }
 
    def apply(docId: String, doc: Iterable[String]): Option[C] = {
 
-     val scorePerKlass = allKlassInfo.keys.map{ klass => (klass,score(klass, doc)) }
+     val scorePerKlass = allKlassInfo.keys.map {klass => (klass,score(klass, doc))}
 
-     if(debug)
+     if (debug)
        println("--scorePerKlass against %s:\n%s".format(docId, scorePerKlass.mkString(", ")))
 
-     if(scorePerKlass.groupBy{ case (klass, score) => score }.size == 1){
-        if(debug)
-          println("--no discrimination for %s".format(docId))
-        None
+     if (scorePerKlass.groupBy {case (klass, score) => score}.size == 1) {
+       if(debug)
+         println("--no discrimination for %s".format(docId))
+       None
      }
      else
        Some(scorePerKlass.maxBy{_._2}._1)
    }
 
-   private def probabilityTermGivenKlass(term: String, klass: C): Double={
+   private def probabilityTermGivenKlass(term: String, klass: C): Double = {
      val klassInfo = allKlassInfo(klass)
      val freq = klassInfo.termFreq.getOrElse(term,0)
-
+     // Laplace add 1 smoothing
      (freq + 1.0)/(klassInfo.nTerms + vocabulary.size)
    }
 
@@ -59,8 +57,8 @@ class NaiveBayes[C](debug: Boolean = false) {
      val klassInfo = allKlassInfo(klass)
      val probabilityKlass = (klassInfo.nDocs + 0.0)/nDocs
 
-     doc.foldLeft(probabilityKlass){ (product,term) => 
-       product*probabilityTermGivenKlass(term, klass)
+     doc.foldLeft(math.log(probabilityKlass)) { (sum,term) => 
+       sum + math.log(probabilityTermGivenKlass(term, klass))
      }
    }
 }
--- src/test/scala/NaiveBayesTest.scala
+++ src/test/scala/NaiveBayesTest.scala
@@ -8,19 +8,19 @@ class NaiveBayesTest extends FunSuite {
      nb.train("china","Chinese Beijing Chinese".split(" "))
      nb.train("other","Tokyo Japan Chinese".split(" "))
 
-     assertResult(4){nb.vocabulary.size}
+     assertResult(4) {nb.vocabulary.size}
 
-     assertResult(2){nb.nDocs}    
+     assertResult(2) {nb.nDocs}    
 
-     assertResult(3){nb.allKlassInfo("china").nTerms}
+     assertResult(3) {nb.allKlassInfo("china").nTerms}
 
-     assertResult(1){nb.allKlassInfo("china").nDocs}
+     assertResult(1) {nb.allKlassInfo("china").nDocs}
 
-     assertResult(2){nb.allKlassInfo("china").termFreq("Chinese")}
+     assertResult(2) {nb.allKlassInfo("china").termFreq("Chinese")}
   }
 
   test("example 13.1") {
-     val nb = new NaiveBayes[String]
+     val nb = new NaiveBayes[String](debug = true)
 
      Map("Chinese Beijing Chinese"->"china",
          "Chinese Chinese Shanghai"->"china",
@@ -29,8 +29,8 @@ class NaiveBayesTest extends FunSuite {
        nb.train(country,str.split(" "))
      }
 
-     assertResult(Some("china")) { nb.apply("1","Chinese Chinese Chinese Tokyo Japan".split(" "))}
-     assertResult(Some("other")) { nb.apply("2","Tokyo Japan".split(" "))}
+     assertResult(Some("china")) {nb.apply("1", "Chinese Chinese Chinese Tokyo Japan".split(" "))}
+     assertResult(Some("other")) {nb.apply("2", "Tokyo Japan".split(" "))}
   }
  
 }
